95012522c1bf4253a9d6ff4a1ea594d205c7ca03
Berkay Çelik <60926774+OBSoft-Berkaycel@users.noreply.github.com>
2024-09-23 15:06:58 +0300

    First Commit

---
+++ android/app/src/main/kotlin/com/example/mytabapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mytabapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    ScrollableCardList(),
+    Center(child: Text('Page 2')),
+    Center(child: Text('Page 3')),
+    Center(child: Text('Page 4')),
+    Center(child: Text('Page 5')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bottom Navigation Example'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: Container(
+        color: Colors.lightBlue[300], // Gökyüzü mavisi
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          unselectedItemColor: Colors.lightBlue[300], // Seçilmemiş item rengi
+          onTap: _onItemTapped,
+          backgroundColor: Colors.transparent, // Arka plan rengini transparan yaptık
+          selectedLabelStyle: TextStyle(color: Colors.amber[800]),
+          unselectedLabelStyle: TextStyle(color: Colors.lightBlue[300]),
+        ),
+      ),
+    );
+  }
+}
+
+class ScrollableCardList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 15,
+      itemBuilder: (context, index) {
+        return Card(
+          margin: EdgeInsets.all(10),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              'Card Item $index',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
